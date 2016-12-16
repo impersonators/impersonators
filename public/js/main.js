55647b00534040f69d0dca7366e7a67ef356c45e
Brynne Zdybel <bazdybel@gmail.com>
2016-12-15 22:48:50 -0500

    updated jack button.  Fixes #39. Fixes #40. Fixes #47.

--- public/js/main.js
+++ public/js/main.js
@@ -7,9 +7,9 @@ window.onload = function(){
 	buttonImage.addEventListener('click', changeContent);
 
 	//set variables for input boxes clear on focus
-	var nameInput = document.getElementsByClassName('contact-content__form__input')[0]
-	var emailInput = document.getElementsByClassName('contact-content__form__input')[1]
-	var messageInput = document.getElementsByClassName('contact-content__form__input__message')[0]
+	var nameInput = document.getElementsByClassName('contact-content__form__input')[0];
+	var emailInput = document.getElementsByClassName('contact-content__form__input')[1];
+	var messageInput = document.getElementsByClassName('contact-content__form__input__message')[0];
 
 	//add onfocus event listener to inbut boxes
 	nameInput.addEventListener('click', function(){
@@ -30,21 +30,33 @@ window.onload = function(){
 
 //change input boxes to greeting on click	
 function changeContent(){
-	//set variable for button image, empty content, submitted content and contact-heading
-	var buttonImage = document.getElementById('contact-content__button__image');
-	var emptyContent= document.getElementById('contact-content__form__empty-content');
-	var submittedContent = document.getElementById('contact-content__form__submitted-content');
-	var contentHeading = document.getElementById('contact-heading');
-
-	emptyContent.style.display = 'none';
-	submittedContent.style.display = 'flex';
-	buttonImage.src = "../images/jack_empty.jpg";
-	contentHeading.innerHTML = " ";
+	//set emailInput for this function
+	var emailInput = document.getElementsByClassName('contact-content__form__input')[1];
+	//if statement for if emailInput contains @
+	if (emailInput.value.search('@') > -1){
+		//set variables of what to change
+		var buttonImage = document.getElementById('contact-content__button__image');
+		var emptyContent = document.getElementById('contact-content__form__empty-content');
+		var submittedContent = document.getElementById('contact-content__form__submitted-content');
+		var contentHeading = document.getElementById('contact-heading');
+		//make change to main content of contact page
+		emptyContent.style.display = 'none';
+		submittedContent.style.display = 'flex';
+		buttonImage.src = "../images/jack_empty.jpg";
+		contentHeading.innerHTML = " ";
+	};
+	//if statement for if emailInput does not contain @
+	if (emailInput.value.search('@') == -1){
+		var emailNotice = document.getElementsByClassName('conact-content__form__email__notice')[0];
+		emailNotice.style.display = "block";
+		valueClear(emailInput);
+	};	
 };
+
+
 
 //change input boxes to clear on focus
 function valueClear(input){
 	input.value = " ";
-
 };
 
