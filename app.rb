5df695a45a970b11e4551ce88ff188acfb265f53
Brynne Zdybel <bazdybel@gmail.com>
2016-12-18 21:45:22 -0500

    fixed email bug by adding erb pages and adding logic in app.rb instead of the js page. Fixes #38.

--- app.rb
+++ app.rb
@@ -51,28 +51,53 @@ get '/gallery' do
   erb :gallery
 end
 
+
+#emailSent and enterEmail have page_title "Contact" to keep local_links correct
+get '/emailSent' do
+	@page_title = "Contact"
+	@local_links = serve_correct_links
+	erb :emailSent
+end
+
+get '/enterEmail' do
+	@page_title = "Contact"
+	@local_links = serve_correct_links
+	erb :enterEmail
+end
+
+
 #naming params from contact page
 #getting SendGrid set up for contact page
 
-post '/contact' do
+post '/contact' do	
 	@page_title = "Contact"
 	@local_links = serve_correct_links
-  	@name = params[:name].upcase
-  	@email = params[:email]
-  	@content = params[:message]
-  	@subject = "Impersonators NYCDA Emails"
+	@name = params[:name].upcase
+	@email = params[:email]
+	@content = params[:message]
+	@subject = "Impersonators NYCDA Emails"
 
-	from = SendGrid::Email.new(email: @email)
-	subject = @subject
-	#below is where you can put your own email to test it
-	to = SendGrid::Email.new(email: 'tester@tester.com')
-	content = SendGrid::Content.new(type: 'text/plain', value: @content)
-	mail = SendGrid::Mail.new(from, subject, to, content)
-	sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-	response = sg.client.mail._('send').post(request_body: mail.to_json)
+	#checking for email to contain @
+	special = '@'
+	puts special
+	regex = /[#{special}]/
+	if @email =~ regex
+	#send email and show emailSent page if @
+		from = SendGrid::Email.new(email: @email)
+		subject = @subject
+		#below is where you can put your own email to test it
+		to = SendGrid::Email.new(email: 'test@test.com')
+		content = SendGrid::Content.new(type: 'text/plain', value: @content)
+		mail = SendGrid::Mail.new(from, subject, to, content)
+		sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+		response = sg.client.mail._('send').post(request_body: mail.to_json)
 
-	puts response.status_code
-	puts response.body
+		erb :emailSent
 
-	erb :contact
+	#don't send email and change to enterEmail if no @
+	else
+		puts @email
+		erb :enterEmail
+	end
 end
+
--- public/js/main.js
+++ public/js/main.js
@@ -4,13 +4,13 @@ window.onload = function(){
 	var nameInput = document.getElementsByClassName('contact-content__form__input')[0];
 	var emailInput = document.getElementsByClassName('contact-content__form__input')[1];
 	var messageInput = document.getElementsByClassName('contact-content__form__input__message')[0];
-	var buttonImage = document.getElementsByClassName('omggmo')[0];
+	var buttonImage = document.getElementsByClassName('button-image')[0];
 
 	if(nameInput != null){
 		//add click to button image(jack image)
-		buttonImage.addEventListener('click', function() {
-			changeContent();
-		});
+		// buttonImage.addEventListener('click', function() {
+		// 	changeContent();
+		// //});
 		//add onfocus event listener to inbut boxes
 		nameInput.addEventListener('click', function(){
 			valueClear(nameInput)
@@ -78,30 +78,30 @@ function hideNav(navDrawer){
 }
 
 
-//change input boxes to greeting on click
-function changeContent(){
-	//set emailInput for this function
-	var emailInput = document.getElementsByClassName('contact-content__form__input')[1];
-	//if statement for if emailInput contains @
-	if (emailInput.value.search('@') > -1){
-		//set variables of what to change
-		var buttonImage = document.getElementById('contact-content__button__image');
-		var emptyContent = document.getElementById('contact-content__form__empty-content');
-		var submittedContent = document.getElementById('contact-content__form__submitted-content');
-		var contentHeading = document.getElementById('contact-heading');
-		//make change to main content of contact page
-		emptyContent.style.display = 'none';
-		submittedContent.style.display = 'flex';
-		buttonImage.src = "../images/jack_empty.jpg";
-		contentHeading.innerHTML = " ";
-	};
-	//if statement for if emailInput does not contain @
-	if (emailInput.value.search('@') == -1){
-		var emailNotice = document.getElementsByClassName('conact-content__form__email__notice')[0];
-		emailNotice.style.display = "block";
-		valueClear(emailInput);
-	};
-};
+// //change input boxes to greeting on click
+// function changeContent(){
+// 	//set emailInput for this function
+// 	var emailInput = document.getElementsByClassName('contact-content__form__input')[1];
+// 	//if statement for if emailInput contains @
+// 	if (emailInput.value.search('@') > -1){
+// 		//set variables of what to change
+// 		var buttonImage = document.getElementById('contact-content__button__image');
+// 		var emptyContent = document.getElementById('contact-content__form__empty-content');
+// 		var submittedContent = document.getElementById('contact-content__form__submitted-content');
+// 		var contentHeading = document.getElementById('contact-heading');
+// 		//make change to main content of contact page
+// 		emptyContent.style.display = 'none';
+// 		submittedContent.style.display = 'flex';
+// 		buttonImage.src = "../images/jack_empty.jpg";
+// 		contentHeading.innerHTML = " ";
+// 	};
+// 	//if statement for if emailInput does not contain @
+// 	if (emailInput.value.search('@') == -1){
+// 		var emailNotice = document.getElementsByClassName('conact-content__form__email__notice')[0];
+// 		emailNotice.style.display = "block";
+// 		valueClear(emailInput);
+// 	};
+// };
 
 
 
